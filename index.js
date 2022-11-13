d117f95ca9a6bf98ac21aadf6d90195ff518d103
Julia <ajulia.fernanda80@gmail.com>
2022-11-13 14:50:25 -0300

    express

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-	const app = express()
-	const path = require('path')
-	const router = express.Router()
+const app = express()
+const path = require('path')
+const router = express.Router()
 
-	router.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname+'/index.html'))
-	})
+router.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname+'/index.html'))
+})
 
-	app.use('/', router)
+app.use('/', router)
 	
-	app.listen(process.env.port || 8080)
+app.listen(process.env.port || 8080)
