6e39f3d7399ad47e5c1054a2693be4a7c2583672
Julia <ajulia.fernanda80@gmail.com>
2022-11-13 15:42:09 -0300

    css

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const path = require('path')
 const router = express.Router()
 
 app.use(express.static("public"))
-router.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname+'/index.html'))
-})
+// router.get('/', function(req, res) {
+// 	res.sendFile(path.join(__dirname+'/index.html'))
+// })
 
 // app.use('/', router)
 	
